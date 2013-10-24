0ab4d5ae5c58ff3718fa36d00085a006bc21b330
hazunigac <hzc91_07@hotmail.com>
2013-10-24 06:42:35 -0600

    ListView Correction and Example files

--- src/quique/proyecto/excercise/JsonActivity.java
+++ src/quique/proyecto/excercise/JsonActivity.java
@@ -1,8 +1,20 @@
 package quique.proyecto.excercise;
 
-import android.os.Bundle;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.content.Context;
+import android.os.Build;
+import android.os.Bundle;
 import android.view.Menu;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class JsonActivity extends Activity {
 
@@ -10,6 +22,18 @@ public class JsonActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_json);
+		final ListView listview = (ListView) findViewById(R.id.listView1);
+	    String[] values = new String[] { "AndroidSpy", "Running From Bombs", "Code Analysis",
+	        "Spy Story", "Mission Quest", "Spy world"};
+
+	    final ArrayList<String> list = new ArrayList<String>();
+	    for (int i = 0; i < values.length; ++i) {
+	      list.add(values[i]);
+	    }
+	    final StableArrayAdapter adapter = new StableArrayAdapter(this,
+	        android.R.layout.simple_list_item_1, list);
+	    listview.setAdapter(adapter);
+
 	}
 
 	@Override
@@ -18,5 +42,29 @@ public class JsonActivity extends Activity {
 		getMenuInflater().inflate(R.menu.json, menu);
 		return true;
 	}
+	
+	 private class StableArrayAdapter extends ArrayAdapter<String> {
 
+		    HashMap<String, Integer> mIdMap = new HashMap<String, Integer>();
+
+		    public StableArrayAdapter(Context context, int textViewResourceId,
+		        List<String> objects) {
+		      super(context, textViewResourceId, objects);
+		      for (int i = 0; i < objects.size(); ++i) {
+		        mIdMap.put(objects.get(i), i);
+		      }
+		    }
+
+		    @Override
+		    public long getItemId(int position) {
+		      String item = getItem(position);
+		      return mIdMap.get(item);
+		    }
+
+		    @Override
+		    public boolean hasStableIds() {
+		      return true;
+		    }
+
+		  }
 }
